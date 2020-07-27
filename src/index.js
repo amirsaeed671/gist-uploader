3e06b25879de3248b70205634e8b861c28d5c12c
Amir Ali <amir.saeed.ali92@gmail.com>
2020-07-27 15:50:16 +0500

    client id testing 🧪

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-
-console.log(process.env.CLIENT_ID)
-
-function App() {
-    return (
-        <input class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="email" placeholder="jane@example.com" />
-    )
-}
+import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
