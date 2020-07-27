c51c3dc91f8c57739cfdf8c786a1ea16d3200fbc
Amir Ali <amir.saeed.ali92@gmail.com>
2020-07-27 18:55:13 +0500

    form component error fixes

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from 'react-dom'
 
 function Form() {
   return (
@@ -11,13 +10,13 @@ function Form() {
             htmlFor="username"
           >
             Username
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Username"
+            />
           </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="username"
-            type="text"
-            placeholder="Username"
-          />
         </div>
         <div className="mb-6">
           <label
@@ -25,13 +24,13 @@ function Form() {
             htmlFor="password"
           >
             Password
+            <input
+              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="******************"
+            />
           </label>
-          <input
-            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            type="password"
-            placeholder="******************"
-          />
           <p className="text-red-500 text-xs italic">
             Please choose a password.
           </p>
@@ -43,12 +42,6 @@ function Form() {
           >
             Sign In
           </button>
-          <a
-            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-            href="#"
-          >
-            Forgot Password?
-          </a>
         </div>
       </form>
       <p className="text-center text-gray-500 text-xs">
