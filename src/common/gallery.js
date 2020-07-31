ed4984e6444189eea4dcfef03307ba9ab2209e51
Amir Ali <amir.saeed.ali92@gmail.com>
2020-07-31 20:10:41 +0500

    iframe component delete

---
+++ src/common/gallery.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+function Gallery() {
+  return (
+    <div className="h-40 bg-gray-100 flex flex-wrap max-w-sm">
+      <div className="rounded-sm shadow-lg bg-pink-300 p-4 m-2">1</div>
+      <div className="rounded-sm shadow-lg bg-pink-300 p-4 m-2">1</div>
+      <div className="rounded-sm shadow-lg bg-pink-300 p-4 m-2">1</div>
+      <div className="rounded-sm shadow-lg bg-pink-300 p-4 m-2">1</div>
+    </div>
+  );
+}
+
+export default Gallery;
